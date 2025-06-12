1708ef8049004ec8a61c1af828dc0d77cdd86f11
Zahid M ERTOGRAL <zahidmertogral@Zahids-MacBook-Air.local>
2025-06-12 23:30:01 +0300

    CI/CD test için küçük güncelleme

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deploy Başarılı!"
+    return "Deploy Başarılı (CI/CD)!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
